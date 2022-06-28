36c09554cd176723d447da18d35d2060d18cc576
rafaalmeida1 <rafaempresarial312@gmail.com>
2022-06-28 18:51:16 -0300

    Add state on NavLink active

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { House, Phone, ShoppingBag, UserList } from "phosphor-react";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
+    const path = window.location.pathname;
+
     return (
         <div className="p-3">
             <header className="max-w-[1110px] flex items-center justify-between pt-2 pb-4 mx-auto">
@@ -15,28 +17,48 @@ export function Header() {
                 <div className="flex gap-3 group">
                     <NavLink
                         to='/'
-                        className={` flex justify-center items-center bg-gray-50 text-gray-900 hover:bg-gray-900 hover:border hover:border-gray-50 hover:text-gray-50 h-[3rem] w-[3rem] rounded-lg  transition-colors duration-300`}
+                        className={` flex justify-center items-center bg-gray-100 text-gray-900 hover:bg-gray-900 hover:border hover:border-gray-100 hover:text-gray-100 h-[3rem] w-[3rem] rounded-lg  transition-colors duration-300`}
+                        style={path === '/' ? { 
+                         backgroundColor: '#09090A',
+                         borderColor: '#E1E1E6',
+                         borderWidth: '1px',
+                         color: '#E1E1E6'
+                        } : {}}
                     >
                         <House size={24} />
                     </NavLink>
 
                     <NavLink
                         to='/about'
-                        className={` flex justify-center items-center bg-gray-50 text-gray-900 hover:bg-gray-900 hover:border hover:border-gray-50 hover:text-gray-50 h-[3rem] w-[3rem] rounded-lg  transition-colors duration-300`}
+                        className={` flex justify-center items-center bg-gray-100 text-gray-900 hover:bg-gray-900 hover:border hover:border-gray-100 hover:text-gray-100 h-[3rem] w-[3rem] rounded-lg  transition-colors duration-300`}
+                        style={path === '/about' ? { 
+                            backgroundColor: '#09090A',
+                            borderColor: '#E1E1E6',
+                            borderWidth: '1px',
+                            color: '#E1E1E6'
+                           } : {}}
                     >
                         <UserList size={24} />
                     </NavLink>
 
                     <NavLink
                         to='/contact'
-                        className={` flex justify-center items-center bg-gray-50 text-gray-900 hover:bg-gray-900 hover:border hover:border-gray-50 hover:text-gray-50 h-[3rem] w-[3rem] rounded-lg  transition-colors duration-300`}
+                        className={` flex justify-center items-center bg-gray-100 text-gray-900 hover:bg-gray-900 hover:border hover:border-gray-100 hover:text-gray-100 h-[3rem] w-[3rem] rounded-lg  transition-colors duration-300`}
+                        style={path === '/contact' ? { 
+                            backgroundColor: '#09090A',
+                            borderColor: '#E1E1E6',
+                            borderWidth: '1px',
+                            color: '#E1E1E6',
+                           } : {}}
                     >
                         <Phone size={24} />
                     </NavLink>
 
                     <a
                         href='https://api.whatsapp.com/send?phone=5519992864222&text=Tenho%20interesse%20em%20seus%20servi%C3%A7os%20de%20Desenvolvimento. '
-                        className={` flex justify-center items-center bg-gray-90 text-gray-50 border border-gray-50 hover:bg-gray-50 hover:border hover:text-gray-900 h-[3rem] w-[5rem] rounded-lg  transition-colors duration-300`}
+                        className={` flex justify-center items-center bg-gradient-to-r from-green-300 to-green-700 text-gray-100 border border-gray-100 
+                            hover:bg-gradient-to-l hover:from-gray-900 hover:border hover:text-green-500  
+                            h-[3rem] w-[5rem] rounded-lg  transition-all duration-75`}
                         target="_blank"
                     >
                         <ShoppingBag size={24} />
